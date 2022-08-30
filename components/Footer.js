b1e00349fb7151631d4267e99648f521abeebe98
kr1st1nagr03g3r <slowest.barges_0i@icloud.com>
2022-08-30 11:13:37 -0400

    tabs

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ import Container from '@mui/material/Container';
 export default function Footer() {
     return (
 
-        <Container maxWidth >
+        <Container  >
             <Container fixed>
                 <p className="legal">This research was funded by Genome Canada and the Canadian Institute for Health Research.</p>
             </Container>
--- pages/research.js
+++ pages/research.js
@@ -1,14 +1,22 @@
 import * as React from 'react';
 import Navigation from '../components/Navigation'
+import Footer from '../components/Footer'
 import PropTypes from 'prop-types';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import Container from '@mui/material/Container'
+import Button from '@mui/material/Button'
+import styles from '../styles/Research.module.css'
+import Image from 'next/image'
+import Images from '../public/images/research/index.js'
+
+
+
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
-
   return (
     <>
 
@@ -51,27 +59,56 @@ export default function BasicTabs() {
 
   return (
     <>
+
       <Navigation />
-      <div>
-        <Box sx={{ width: '100%' }}>
-          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-            <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-              <Tab label="Item One" {...a11yProps(0)} />
-              <Tab label="Item Two" {...a11yProps(1)} />
-              <Tab label="Item Three" {...a11yProps(2)} />
+      <Container className="center">
+        <h2 className="title">Model Overview</h2>
+
+
+        <p className="para">Linear models of pharmaceutical innovation and commercialization are inadequate and did not capture the complexity of the data we acquired. For example, the classic “pipeline model” of drug development demonstrates the sequence of events necessary for translating basic research from “bench to bedside,” however it fails to account for the elaborate systems of communication, legal and regulatory framework, financial inputs and outputs, and scientific hurdles that shape and determine the path of innovation in this sector.
+        </p>
+
+        <p className="para">
+          We use the metaphor of a roadmap to demonstrate that translational medicine occurs sequentially but not necessarily linearly. This overview of the road to pharmaceutical innovation visualizes the institutions along the route, the stakeholders moving along the road, and the roadblocks and bottlenecks that impede progress. This is an active ecosystem where the social and scientific are inherently enmeshed.
+        </p>
+        <Button className="button" href="/research#items">Learn More</Button>
+      </Container>
+
+
+
+      <Container className={styles.tabs}>
+        <Box id="items" sx={{ width: '100%' }}>
+          <Box sx={{ borderBottom: 0, borderColor: 'divider', }}>
+            <Tabs value={value} onChange={handleChange} aria-label="basic tabs" >
+              <Tab sx={{ textTransform: 'none', color: '#ffffff' }} label="Roadmap" {...a11yProps(0)} />
+              <Tab sx={{ textTransform: 'none', color: '#ffffff' }} label="Roadblocks" {...a11yProps(1)} />
+              <Tab sx={{ textTransform: 'none', color: '#ffffff' }} label="Item Three" {...a11yProps(2)} />
             </Tabs>
           </Box>
           <TabPanel value={value} index={0}>
-            Item One
+
+            <p className="para">Roads represent...
+
+              Ullamco tempor ad excepteur Lorem consectetur ea eiusmod do. Ut quis ullamco exercitation laborum occaecat veniam incididunt. Incididunt commodo aute est nostrud excepteur enim non fugiat dolore incididunt culpa ut. Proident nulla fugiat in enim exercitation anim eiusmod duis dolor aliqua excepteur aliquip. Dolore dolor Lorem voluptate ea non nulla mollit ipsum.
+              <br></br><br></br>
+
+              <Container fixed className={styles.research}>
+                <Box className={styles.wrapper}>
+                  <Image src={Images.researchBackground} alt="green grass background for many buildings" />
+                </Box>
+              </Container>
+
+            </p>
           </TabPanel>
           <TabPanel value={value} index={1}>
-            Item Two
+            Roadblocks
           </TabPanel>
           <TabPanel value={value} index={2}>
             Item Three
           </TabPanel>
         </Box>
-      </div>
+      </Container>
+      <Footer />
     </>
   );
 }
