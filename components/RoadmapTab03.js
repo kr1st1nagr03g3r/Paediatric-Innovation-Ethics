08c8ae8c393c656d53ea355df27987040f1fde95
Kristina Groeger <slowest.barges_0i@icloud.com>
2023-04-03 16:08:06 -0400

    Updating buildings and institutions

--- components/RoadmapTab03.js
+++ components/RoadmapTab03.js
@@ -1,11 +1,11 @@
-import * as React from 'react'
-import styles from '../styles/Research.module.css'
-import Images from '../public/images/research/index'
-import Image from 'next/image'
-import { Box, Button, Container } from '@mui/material'
-import ImageTest from './Barriers'
-import Barriers from './Barriers'
-import Link from 'next/link'
+import * as React from "react";
+import styles from "../styles/Research.module.css";
+import Images from "../public/images/research/index";
+import Image from "next/image";
+import { Box, Button, Container } from "@mui/material";
+import ImageTest from "./Barriers";
+import Barriers from "./Barriers";
+import Link from "next/link";
 
 // const HtmlTooltip = styled(({ className, ...props }) => (
 //   <Tooltip {...props} classes={{ popper: className }} />
@@ -22,116 +22,190 @@ import Link from 'next/link'
 // }))
 
 export default function RoadmapTab02() {
-  return (
-    <>
-      <style jsx>{`
-        .relative {
-          position: relative;
-          display: block;
-        }
-        .absolute {
-          position: absolute;
-        }
+return (
+<>
+<style jsx>{`
+.relative {
+position: relative;
+display: block;
+}
+.absolute {
+position: absolute;
+}
 
-           span.hoverInfo {
-  display: flex;
+span.hoverInfo {
+display: flex;
 }
 span.centerButton {
-  display: none !important;
+display: none !important;
 }
 
 .relative {
- position: relative;
+position: relative;
 }
 
 .absolute {
-  position: absolute;
+position: absolute;
 }
 
 .redLink {
-  height: 100px;
-  width: 1000px;
-  z-index: 1000;
-  top: 0;
-  left: 0;
-  position: absolute;
+height: 100px;
+width: 100%;
+z-index: 1000;
+top: 0;
+left: 0;
+position: absolute;
 }
 
 .gradientLink {
-    height: 100px;
-  width: 1000px;
-  z-index: 1000;
-  top: 0;
-  left: 0;
-  position: absolute;
-      margin-top: 110px;
+height: 100px;
+width: 100%;
+z-index: 1000;
+top: 110px;
+left: 0;
+position: absolute;
 }
 .purpleLink {
-    height: 100px;
-  width: 1000px;
-  z-index: 1000;
-  top: 0;
-  left: 0;
-  position: absolute;
-      margin-top: 410px;
+height: 100px;
+width: 100%;
+z-index: 1000;
+top: 508px;
+left: 0;
+position: absolute;
 }
 
 .greenLink {
 height: 200px;
-width: 1000px;
-    z-index: 1000;
-    top: 0;
-    left: 0;
-    position: absolute;
-    margin-top: 529px;
+width: 100%;
+z-index: 1000;
+top: 667px;
+left: 0;
+position: absolute;
 }
 
 .blueLink {
-    height: 200px;
-    width: 200px;
-    z-index: 1000;
-    top: 0;
-    left: 0;
-    position: absolute;
-    margin-top: 214px;
-    margin-left: 200px;
+height: 200px;
+width: 200px;
+z-index: 1000;
+top: 214px;
+left: 39px;
+position: absolute;
 }
 
 .yellowLink {
-    height: 200px;
-    width: 200px;
-
-    z-index: 1000;
-    top: 0;
-    left: 0;
-    position: absolute;
-    margin-top: 214px;
-    margin-left: 420px;
+  height: 177px;
+  width: 200px;
+  z-index: 1000;
+  top: 300px;
+  left: 420px;
+  position: absolute;
 }
 
 .orangeLink {
-    height: 200px;
-    width: 200px;
-   
-    z-index: 1000;
-    top: 0;
-    left: 0;
-    position: absolute;
-    margin-top: 212px;
-    margin-left: 689px;
+height: 177px;
+width: 200px;
+z-index: 1000;
+top: 300px;
+left:  689px;
+position: absolute;
 }
 
-          @media only screen and (max-width: 1200px) {
+@media only screen and (max-width: 1024px) {
+.yellowLink {
+height: 139px;
+width: 200px;
+top: 216px;
+left: 254px;
+}
+
+.orangeLink {
+height: 177px;
+width: 200px;
+top: 211px;
+left: 529px;
+}
+
+.greenLink {
+top: 543px;
+}
+}
+
+
+
+
+
+@media only screen and (max-width: 768px) {
+.redLink {
+height: 58px;
+}
+
+.gradientLink {
+height: 100px;
+width: 80%;
+z-index: 1000;
+top: 62px;
+right: 0;
+position: absolute;
+left: unset;
+}
+
+.purpleLink {
+top: 248px;
+}
+
+.blueLink {
+height: 100px;
+width: 100px;
+z-index: 1000;
+top: 128px;
+left: 39px;
+}
+
+.yellowLink {
+height: 123px;
+width: 100px;
+top: 126px;
+left: 205px;
+}
+
+.orangeLink {
+height: 100px;
+width: 100px;
+top: 135px;
+left: 384px;
+}
+
+.greenLink {
+height: 161px;
+width: 100%;
+z-index: 1000;
+top: 341px;
+left: 0;
+position: absolute;
+}
+}
+
+@media only screen and (max-width: 425px) {
+  .orangeLink, .purpleLink, .gradientLink, .blueLink, .greenLink, .yellowLink {
+   width: 0px !Important;
+  }
+  .redLink {
+    height: 230px:
+  }
+}
+
+
+@media only screen and (max-width: 1200px) {
 span.hoverInfo {
-  display: none !important;
+display: none !important;
 }
 span.centerButton.center {
-  padding: 20px 0px;
-  display: flex !important;
+padding: 20px 0px;
+display: flex !important;
 }
-          }
-        }
-      `}</style>
+}
+}
+`}</style>
       <div>
         <Container maxWidth="lg">
           <p>
@@ -174,5 +248,5 @@ span.centerButton.center {
         </Container>
       </div>
     </>
-  )
+  );
 }
